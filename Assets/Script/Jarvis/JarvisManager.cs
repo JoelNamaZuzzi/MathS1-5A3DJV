812aa0950130e4734be5b3ca3efc9bfc09414ffe
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-11-12 18:23:38 +0100

    Added mats + find lowest X for Jarvis
    Put some materials and added the function to find the lowest X, now going for some vector 2

--- Assets/Script/Jarvis/JarvisManager.cs
+++ Assets/Script/Jarvis/JarvisManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,38 @@ using UnityEngine;
 public class JarvisManager : MonoBehaviour
 {
     public List<GameObject> Points = new List<GameObject>();
+    private GameObject CurPts;
+
+    private void Start()
+    {
+        FindLowestX();
+    }
+
+    public void FindLowestX()
+    {
+        float curlowX=10000.0f;
+        float curlowY = 10000.0f;
+        foreach (GameObject pts in Points)
+        {
+            if (pts.transform.position.x < curlowX)
+            {
+                CurPts = pts;
+                var position = CurPts.transform.position;
+                curlowX = position.x;
+                curlowY = position.y;
+                Debug.Log(CurPts.name);
+            }
+            else if (Math.Abs(pts.transform.position.x - curlowX) < 0.1f)
+            {
+                if (pts.transform.position.y < curlowY)
+                {
+                    CurPts = pts;
+                    var position = CurPts.transform.position;
+                    curlowX = position.x;
+                    curlowY = position.y;
+                    Debug.Log(CurPts.name);
+                }
+            }
+        }
+    }
 }

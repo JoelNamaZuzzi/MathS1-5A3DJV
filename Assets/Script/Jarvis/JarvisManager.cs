faaac7022f34327373a309b4f60886c0a45f1545
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-11-16 10:12:03 +0100

    Updated merge

--- Assets/Script/Jarvis/JarvisManager.cs
+++ Assets/Script/Jarvis/JarvisManager.cs
@@ -12,7 +12,7 @@ public class JarvisManager : MonoBehaviour
     private GameObject endPoint=null;
     private void Start()
     {
-        JarvisWalk();
+        //JarvisWalk();
     }
 
     public GameObject FindLowestX()

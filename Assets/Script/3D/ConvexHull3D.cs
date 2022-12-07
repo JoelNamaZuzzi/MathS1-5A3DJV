1bbbfac769991261459d45dbe36547ee6ea11dc3
rvejus <76257185+rvejus@users.noreply.github.com>
2022-12-07 20:16:09 +0100

    correction tetrahedre

--- Assets/Script/3D/ConvexHull3D.cs
+++ Assets/Script/3D/ConvexHull3D.cs
@@ -43,9 +43,40 @@ public class ConvexHull3D : MonoBehaviour
         Triangle triangle3 = new Triangle(listePoints[0],listePoints[2],listePoints[3]);
         EdgesNTris.drawTri(triangle3, Meshobj3);
         
+        GameObject Meshobj4 = Instantiate(meshObj, new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));
+        Triangle triangle4 = new Triangle(listePoints[1],listePoints[2],listePoints[3]);
+        EdgesNTris.drawTri(triangle4, Meshobj3);
+        
         hull.listFace.Add(triangle1);
         hull.listFace.Add(triangle2);
         hull.listFace.Add(triangle3);
+        hull.listFace.Add(triangle4);
+        
+        hull.listEdges.Add(triangle1.edges1);
+        hull.listEdges.Add(triangle1.edges2);
+        hull.listEdges.Add(triangle1.edges3);
+        hull.listEdges.Add(triangle2.edges1);
+        hull.listEdges.Add(triangle2.edges2);
+        hull.listEdges.Add(triangle2.edges3);
+        hull.listEdges.Add(triangle3.edges1);
+        hull.listEdges.Add(triangle3.edges2);
+        hull.listEdges.Add(triangle3.edges3);
+        hull.listEdges.Add(triangle4.edges1);
+        hull.listEdges.Add(triangle4.edges2);
+        hull.listEdges.Add(triangle4.edges3);
+        
+        hull.listPoints.Add(triangle1.point1.transform.position);
+        hull.listPoints.Add(triangle1.point2.transform.position);
+        hull.listPoints.Add(triangle1.point3.transform.position);
+        hull.listPoints.Add(triangle2.point1.transform.position);
+        hull.listPoints.Add(triangle2.point2.transform.position);
+        hull.listPoints.Add(triangle2.point3.transform.position);
+        hull.listPoints.Add(triangle3.point1.transform.position);
+        hull.listPoints.Add(triangle3.point2.transform.position);
+        hull.listPoints.Add(triangle3.point3.transform.position);
+        hull.listPoints.Add(triangle4.point1.transform.position);
+        hull.listPoints.Add(triangle4.point2.transform.position);
+        hull.listPoints.Add(triangle4.point3.transform.position);
     }
 
     bool IsInsidePolygone(GameObject point)
